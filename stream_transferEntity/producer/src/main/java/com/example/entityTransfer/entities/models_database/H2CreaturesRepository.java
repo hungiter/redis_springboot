c234350654937b7a1b4e1f85d306c9d921c8d8b8
Zion Nguyễn <94814450+hungiter@users.noreply.github.com>
2024-05-30 16:31:42 +0700

    Update Producer
    Models/DTOs (modify (skills type)/ new value(power) )
    DB removed

--- stream_transferEntity/producer/src/main/java/com/example/entityTransfer/entities/models_database/H2CreaturesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.entityTransfer.entities.models_database;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface H2CreaturesRepository extends CrudRepository<H2Creatures, String> {
-}
--- stream_transferEntity/producer/src/main/java/com/example/entityTransfer/entities/models_database/H2TribesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.entityTransfer.entities.models_database;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface H2TribesRepository extends CrudRepository<H2Tribes, String> {
-}
